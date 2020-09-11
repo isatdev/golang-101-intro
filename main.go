d25e5339756c77af331d53036e3e183346cbeb29
Imam Kurniawan <imam.kurniawan@indosatooredoo.com>
2020-09-11 15:23:44 +0700

    three - http

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
 
 // Animal struct
 type Animal struct {
@@ -25,6 +31,12 @@ func (a *Animal) tryFly() string {
 	return fmt.Sprintf("A %s can't fly!", a.Name)
 }
 
+// Page struct
+type Page struct {
+	Title string
+	Body  []byte
+}
+
 func main() {
 	fmt.Println("Hello World")
 
@@ -35,4 +47,29 @@ func main() {
 	var duck *Animal
 	duck = NewAnimal(false, 2, "donald")
 	fmt.Printf("a %s Can fly? %s\n", duck.Name, duck.tryFly())
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello World\n"))
+		fmt.Fprintf(res, "Welcome! accesssing route %s", req.URL.Path)
+	})
+	http.HandleFunc("/view/", viewHandler)
+
+	log.Println("Listening to :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func viewHandler(res http.ResponseWriter, req *http.Request) {
+	log.Printf("[%s] %s", req.Method, req.URL.Path)
+	title := req.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(res, "<h1>%s</h1><p>%s</p>", p.Title, p.Body)
+}
+
+func loadPage(p string) (*Page, error) {
+	fname := fmt.Sprintf("res/%s.txt", p)
+	body, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: strings.Title(p), Body: body}, nil
 }
